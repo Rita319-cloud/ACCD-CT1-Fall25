660633625c91712ed00fd5ad68ba53793e8e6319
Rita <pzhang5@inside.artcenter.edu>
2025-10-17 09:34:17 -0700

    1017

---
+++ week4/homework/scripts/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(100, 100);
+}
+
+function draw() {
+    background(220, 0, 0);
+}
+
+// HELPER STUFF:
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
